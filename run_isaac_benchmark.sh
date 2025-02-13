bbc239be6fd10b12810f4b3437e495c5776f8dc4
david-s-martinez <dsmartinez13@hotmail.com>
2025-02-13 23:25:16 +0100

    feat: improved bash script for benchmark

--- run_isaac_benchmark.sh
+++ run_isaac_benchmark.sh
@@ -1,9 +1,11 @@
 sudo chmod -R 777 /home/$USER/f3rm/datasets/eyeinhand_nerf1/benchmark
+conda deactivate
 ~/.local/share/ov/pkg/isaac-sim-2023.1.0-hotfix.1/python.sh ./standalone_benchmark.py \
     --json_dir=/home/$USER/f3rm/datasets/eyeinhand_nerf1/benchmark \
     --gripper_dir=/home/$USER/isaac_sim_grasping/grippers \
     --objects_dir=/home/$USER/isaac_sim_grasping/gazebo-objects/objects_gazebo/ycb \
     --output_dir=/home/$USER/Documents/Dataset/hithand_filtered \
+    --headless\
     --num_w=50 \
     --test_time=3 \
     --controller=default \
